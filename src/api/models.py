9bc61ebbde0b75cdfc8bc185e8e32b1210cdc64b
ImFineZ <greivinrodriguez@icloud.com>
2022-11-01 00:51:45 +0000

    test

--- src/api/models.py
+++ src/api/models.py
@@ -5,6 +5,7 @@ db = SQLAlchemy()
 
 
 # ---------------------------User----------------------------
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), nullable=False)
